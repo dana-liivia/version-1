82e578112fb9eae554c9ed4caa81817789ec4a97
Feuz Dana Livia <feuzd1@bfh.ch>
2024-12-23 22:54:16 +0100

    making header responsive

--- original/src/app/components/header.tsx
+++ original/src/app/components/header.tsx
@@ -3,8 +3,13 @@ import style from "@/app/components/stylings/header.module.css";
 import {useEffect, useState} from "react";
 
 export default function Header() {
-
     const [activeSection, setActiveSection] = useState("");
+    const [isDisplayed, setIsDisplayed] = useState(true);
+
+    useEffect(() => {
+        window.innerWidth < 769 ? setIsDisplayed(false) : setIsDisplayed(true);
+    }, []);
+
 
     useEffect(() => {
         const sections = document.querySelectorAll("section");
@@ -27,20 +32,32 @@ export default function Header() {
 
 
     return (
-        <div className={style.container}>
-            <img src={"logo.png"} alt="logo"/>
-            <a href="#about-me" className={`${style.button} ${activeSection == "about-me" ? style.active : ""}`}>
-                <p className={style.sectionName}>about me</p>
-            </a>
-            <a href="#skills" className={`${style.button} ${activeSection === "skills" ? style.active : ""}`}>
-                <p className={style.sectionName}>skills</p>
-            </a>
-            <a href="#experiences" className={`${style.button} ${activeSection === "experiences" ? style.active : ""}`}>
-                <p className={style.sectionName}>experiences</p>
-            </a>
-            <a href="#hobbies" className={`${style.button} ${activeSection === "hobbies" ? style.active : ""}`}>
-                <p className={style.sectionName}>things</p>
-            </a>
+        <div>
+            {isDisplayed ? (
+                // normal navbar
+                <div className={style.container}>
+                    <img src={"logo.png"} alt="logo"/>
+                    <a href="#about-me"
+                       className={`${style.button} ${activeSection == "about-me" ? style.active : ""}`}>
+                        <p className={style.sectionName}>about me</p>
+                    </a>
+                    <a href="#skills"
+                       className={`${style.button} ${activeSection === "skills" ? style.active : ""}`}>
+                        <p className={style.sectionName}>skills</p>
+                    </a>
+                    <a href="#experiences"
+                       className={`${style.button} ${activeSection === "experiences" ? style.active : ""}`}>
+                        <p className={style.sectionName}>experiences</p>
+                    </a>
+                    <a href="#hobbies"
+                       className={`${style.button} ${activeSection === "hobbies" ? style.active : ""}`}>
+                        <p className={style.sectionName}>things</p>
+                    </a>
+                </div>
+            ) : (
+                // hamburger menu for small screens
+                <div></div>
+            )}
         </div>
     );
 }
